83187addb6a17d07cba374baa591646279e82464
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2020-10-05 22:21:45 +0530

    Hall of Fame

--- src/components/main.js
+++ src/components/main.js
@@ -21,7 +21,11 @@ class Main extends Component {
         <Switch location={this.props.location}>
           <Route exact path='/livestats' component={() => <LiveStats />} />
           <Route exact path='/feedback' component={() => <FeedbackModal />} />
-          <Route exact path='/ourteam' component={() => <Ourteam />} />
+          <Route
+            exact
+            path='/registration'
+            component={() => <Registration />}
+          />
           <Route exact path='/halloffame' component={() => <Hall />} />
           <Route exact path='/example' component={() => <FeedbackModal />} />
           <Route exact path='/questions/:code' component={ShowwithId} />
