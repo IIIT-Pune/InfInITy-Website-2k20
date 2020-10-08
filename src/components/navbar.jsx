905087bfb38a7ffa6b991a0d71e084c05a83e08d
Mukhrit <54068781+Mukhrit@users.noreply.github.com>
2020-10-09 00:24:24 +0530

    ques card page styled modal location changed

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,43 +1,43 @@
-import React from "react";
-import "../assets/scss/navbar.scss";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import '../assets/scss/navbar.scss';
+import { NavLink } from 'react-router-dom';
 
 function Navbar() {
   return (
     <nav>
-      <ul className="navbar">
-        <li className="logo-con">
-          <NavLink exact className="--item logo" to="/">
-            <img src={require("../assets/img/asset_2.png")} alt="" />
+      <ul className='navbar'>
+        <li className='logo-con'>
+          <NavLink exact className='--item logo' to='/'>
+            <img src={require('../assets/img/asset_2.png')} alt='' />
           </NavLink>
         </li>
         <li>
-          <NavLink exact className="--item" to="/">
-            <img src={require("../assets/img/asset_1.png")} alt="" />
+          <NavLink exact className='--item' to='/'>
+            <img src={require('../assets/img/asset_1.png')} alt='' />
             <div>Home</div>
           </NavLink>
         </li>
         <li>
-          <NavLink className="--item" to="/registration">
-            <img src={require("../assets/img/asset_4.png")} alt="" />
+          <NavLink className='--item' to='/registration'>
+            <img src={require('../assets/img/asset_4.png')} alt='' />
             <div>Registration</div>
           </NavLink>
         </li>
         <li>
-          <NavLink className="--item" to="/ourteam">
-            <img src={require("../assets/img/asset_3.png")} alt="" />
+          <NavLink className='--item' to='/ourteam'>
+            <img src={require('../assets/img/asset_3.png')} alt='' />
             <div>Our Team</div>
           </NavLink>
         </li>
         <li>
-          <NavLink className="--item" to="/previously">
-            <img src={require("../assets/img/asset_6.png")} alt="" />
+          <NavLink className='--item' to='/previously'>
+            <img src={require('../assets/img/asset_6.png')} alt='' />
             <div>Previously</div>
           </NavLink>
         </li>
         <li>
-          <NavLink className="--item" to="/halloffame">
-            <img src={require("../assets/img/asset_5.png")} alt="" />
+          <NavLink className='--item' to='/halloffame'>
+            <img src={require('../assets/img/asset_5.png')} alt='' />
             <div>Hall of Fame</div>
           </NavLink>
         </li>
--- src/components/questioncards.js
+++ src/components/questioncards.js
@@ -10,7 +10,13 @@ class QuesCard extends Component {
   render() {
     console.log(questions);
     return (
-      <div style={{ backgroundColor: '#FFFFCC' }}>
+      <div
+        style={{
+          backgroundColor: '#FFFFFF',
+          backgroundImage:
+            'linear-gradient(180deg, #FFFFFF 0%, #6284FF 50%, #FF0000 100%)',
+        }}
+      >
         <div class='textparent'>
           <div class='row fancy-grid' style={{ justifyContent: 'center' }}>
             <div class='fancy-heading'>
@@ -21,7 +27,7 @@ class QuesCard extends Component {
                     'url(https://themegoods-cdn-pzbycso8wng.stackpathdns.com/musico/demo/wp-content/uploads/2018/10/ruvim-noga-711147-unsplash.jpg)',
                 }}
               >
-                2019
+                2k19
               </h1>
             </div>
           </div>
@@ -30,13 +36,17 @@ class QuesCard extends Component {
           <div class='row justify-content-center'>
             {questions.map((obj) => {
               return (
-                <div class='col-lg-3' style={{ padding: '60px' }}>
+                <div
+                  class='col-lg-3 animate__animated animate__bounceIn'
+                  style={{ padding: '60px' }}
+                >
                   <a href='#'>
                     <figure
                       onClick={(event) => {
                         var urlnew = '/questions/' + obj.code;
                         return (window.location.href = urlnew);
                       }}
+                      style={{ backgroundImage: 'url(' + obj.url + ')' }}
                     >
                       <figcaption>
                         <h4>
